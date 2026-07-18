322e27c25a44ae87a82095b766b333d3bf426255
SaravanaPandianChandramohan <sc2590@live.mdx.ac.uk>
2026-07-18 01:36:50 +0100

    Allow external access

--- app.py
+++ app.py
@@ -360,5 +360,8 @@ def complete_donation(donation_id):
     return redirect("/volunteer")
 # ---------------- RUN ---------------- #
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
